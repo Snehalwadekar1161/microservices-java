cf09c250cf11a439db50413cf033f70ae4059601
snehal <snehalwadekar1161@gmail.com>
2023-04-23 23:01:41 +0530

    my first git files commite to github

---
+++ src/main/java/com/example/datajpa/SpringdatajpaegApplication.java
@@ -0,0 +1,14 @@
+package com.example.datajpa;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaegApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/datajpa/service/PostServiceImpl.java
@@ -0,0 +1,61 @@
+package com.example.datajpa.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.datajpa.model.Post;
+import com.example.datajpa.dao.PostDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PostServiceImpl implements PostService {
+
+	@Autowired
+	PostDao dao;
+	
+	@Override
+	public List<Post> getAllPosts() {
+		// TODO Auto-generated method stub
+		return dao.findAll();
+	}
+
+	@Override
+	public Post insertPost(Post post) {
+		// TODO Auto-generated method stub
+		return dao.save(post);
+	}
+
+	@Override
+	public Optional<Post> findPost(Integer pid) {
+		// TODO Auto-generated method stub
+		return dao.findById(pid);
+	}
+
+	@Override
+	public void deletePost(Integer pid) {
+		// TODO Auto-generated method stub
+		dao.deleteById(pid);
+	}
+
+	@Override
+	public List<Post> findByAuthor(String author) {
+		// TODO Auto-generated method stub
+		return dao.findByAuthor(author);
+	}
+
+	@Override
+	public List<Post> findByTitle(String title) {
+		// TODO Auto-generated method stub
+		return dao.findByTitle(title);
+	}
+
+	@Override
+	public List<Post> findByTitleAuthor(String title, String author) {
+		// TODO Auto-generated method stub
+		return dao.findByTitleAndAuthor(title, author);
+	}
+
+}
